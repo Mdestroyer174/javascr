cfddde57c698bf90b141e98427b5d7bd19383ff6
michael <garcia.michael.mrm@gmail.com>
2024-05-06 10:43:35 +0200

    cositas jsc

--- tamagochijs/main.js
+++ tamagochijs/main.js
@@ -5,5 +5,70 @@ function x25() {
 
   function xmax() {
     var element = document.getElementById("progress");
+    element.classList.toggle("progressmax");
+    var x = document.getElementById("bub");
+    var y = document.getElementById("bubh");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+  }
+
+  function x25f() {
+    var element = document.getElementById("progress2");
     element.classList.toggle("progressX25");
   }
+
+  function xmaxf() {
+    var element = document.getElementById("progress2");
+    element.classList.toggle("progressmax");
+    var x = document.getElementById("bub");
+    var y = document.getElementById("bubf");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+  }
+
+  function x25e() {
+    var element = document.getElementById("progress3");
+    element.classList.toggle("progressX25");
+    var x = document.getElementById("bub");
+    var y = document.getElementById("bube");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+  }
+
+  function xmaxe() {
+    var element = document.getElementById("progress3");
+    element.classList.toggle("progressmax");
+   
+  }
+
+  // function fbub() {
+  //   var element2 = document.getElementById("bub");
+  //   element.classList.toggle("bubf"); 
+  // }
